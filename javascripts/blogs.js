b8b4c510bac697f37e6cd6ebd78cef1f62d91b0e
Nathan Pabst <nathan.pabst@gmail.com>
2018-03-30 19:26:45 -0500

    converted js file to ES6, created the XHR request, and moved the blogs object into a JSON file

--- javascripts/blogs.js
+++ javascripts/blogs.js
@@ -1,45 +1,36 @@
-var blogs = [
-    {
-        id: "blog1",
-        title: "Week One",
-        date: "02/27/2018",
-        post: "Our focus this week, in addition to HTML and CSS, was the Command Line and GitHub. Truth be told, I didn't know either existed until a few months ago. So needlesss to say, it has been an eye-opening seven days. The Command Line...whoa. I thought we were past the black-screen DOS days, but I'm learning that it is actually a very powerful tool that offers more control, access to applications that are otherwise unavailable, and the ability to control the mind of servers through scripts. I also learned a few funny acronyms like 'man command', 'touch' and 'mkdir'. It's not a folder, Bill... It's a directory! We can't and won't let you win this battle. ...GitHub... HTML is my pal and I'm beginning to understand how to name and contain the elements so that they can be styled later, but CSS--and more specifically--positioning elements on a page is seemingly the end of me." 
-    },
-    {
-        id: "blog2",
-        title: "Week Three",
-        date: "03/06/2018",
-        post: "Over the past 7 days we've begun learning about strings, conditional statements, arrays, for loops, and objects. <em>Strings</em> There are 6 primative types of strings including: strings, numbers, boolean, null, undefined, and objects--which also include arrays and lists. We learned several new methods to use alongside strings like: indexOf(), replace(), join() and split(), how to concatenate strings, and how to test our code using the console before connecting our JavaScript to the DOM. <em>Conditional Statements</em>: These statements include: if, if else, and else and were properly drilled into my brain from the pre-work. <em>Arrays</em> Arrays are like storage bins that you can hold strings, numbers, and booleans. Through our presentations and challenges, we covered several basic array methods for example: pop()--which removes a value from the end of the array and push()-which adds a value to the beginning of an array. <em>For Loops</em> For loops include 3 parts: the starting value, the stopping value,and how to increment the loop. I was pretty comfortable with these too as a result of the pre-work. This week was incredibly challenging for me. To say that I feel behind the curve, I think, would be an understatement. The best way to describe this week is to say that it's been humbling, overwhelming, and frustrating. With that said, I also understand that this is a 'bootcamp' and is intended to break people again and again in order to adequately prepare them for real-world experience. I also believe that these concepts will sink in as I get more training and more time passes. One thing I found very interesting this week, is that you can solve a problem in any number of ways, but there is definitely a 'wrong answer'. Looking forward to week 4."
-    },
-    {
-        id: "blog3",
-        title: "Week Four",
-        date: "03/12/18",
-        post: "This week we covered advanced JavaScript and GitHub for teams. Functions are starting to make a little more sense. Although, I still get hung up on small issues like naming the variables and parameters. We also watched Zoe, Callan, and Lauren use GitHub to create and manage a project, which was pretty cool. I learned how to work with a team to plan a project and then use GitHub as a project management tool. For example, creating a project, adding issues and styling tickets to include the story, acceptance criteria, and technical notes. For our group project, we only did the planning stage together. Next week we will begin splitting tickets among the team and reviewing each others work, which should be very interesting." 
-    },
-    {
-        id: "blog4",
-        title: "Week Six",
-        date: "3/24/18",
-        post: "This week we covered ES6, DOM interaction and markdown. The new ES6 features will take some time to adjust to, but I'm sure it will be worth it in terms of ease of use, simplification, etc. DOM interaction is a mind-blowing concept..so cool! This concept allows us to respond to how the user is interacting with the page. I learned the hard way the importance of properly placed event listeners. Event listeners allow us to keep our JavaScript from running until the user has had a chance to interact with a particular event, such as keyboard and mouse events. Additionally, we learned how to target different elements in our HTML. For example, using the parentNode attribute to target children and descendents. Last, but not least, we covered Markdown. More specifically, formatting and including content that will make our README.md files look professional. With each passing week, and new topic learned, I am amazed at the number of tools available to developers. There is so much more this it than coding. Excited to see what's next."
-    },
-]
-// Create two functions:
-// writeToDom: this function should accept a string and print it to a div with an id of my-projects
-// createBlogPosts: this function should loop through the BLOGS array and build up the html for each blog. It should call the writeToDom function for each blog.
-function writeToDom(domString, domId){
-    var myDiv = document.getElementById(domId);
-    myDiv.innerHTML += domString;
+const writeToDom = (domString, divId) => {
+    document.getElementById(divId).innerHTML += blogsString;
+};
+
+const createBlogPosts = (myBlogsArray) => {
+    let domString = "";
+    myBlogsArray.forEach((blog) => {
+        domString += `<div id="blog-'+ (i+1) +'">`;
+        domString +=   `<h3>${blogs.title}</h3>`;
+        domString +=   `<h4>${blogs.date}</h4>`;
+        domString +=   `<p>${blogs.post}</p>`;
+        domString += `</div>`
+    });           
+    writeToDom(domString, "blogs");
+};
+
+createBlogPosts(blog);
+
+function executeThisFunctionAfterFileLoads (){
+    const data = JSON.parse(this.responseText);
+    createBlogPosts(data.blogs);
 }
-function createBlogPosts(myBlogs){
-    for(var i = 0; i < myBlogs.length; i++){
-        var blogsString = "";
-           blogsString += '<div id="blog-'+ (i+1) +'">'
-           blogsString +=   '<h3>' + myBlogs[i].title + '</h3>';
-           blogsString +=   '<h4>' + myBlogs[i].date + '</h4>';
-           blogsString +=   '<p>' + myBlogs[i].post + '</p>';
-           blogsString += '</div>'
-        writeToDom(blogsString, "blogs");
-    }
-}    
-createBlogPosts(blogs);
+
+function WTF(){
+    console.log('Oops! Something went wrong.');
+}
+
+const startApplication = () => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener("load", executeThisFunctionAfterFileLoads);
+    myRequest.addEventListener("error", WTF);
+    myRequest.open("GET", "blogs.json");
+    myRequest.send();
+};
+
+startApplication();
