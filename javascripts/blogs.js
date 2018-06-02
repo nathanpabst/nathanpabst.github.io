efec090718ddaab1cfb645061338572064b5d970
Nathan Pabst <nathan.pabst@gmail.com>
2018-06-01 20:03:59 -0500

    added task runners

--- javascripts/blogs.js
+++ javascripts/blogs.js
@@ -1,34 +1,16 @@
-const writeToDom = (domString, divId) => {
-    document.getElementById(divId).innerHTML += domString;
-};
+// function executeThisFunctionAfterFileLoads (){
+//     const data = JSON.parse(this.responseText);
+//     createBlogPosts(data.blogs);
+// }
 
-const createBlogPosts = (myBlogsArray) => {
-    let domString = "";
-    myBlogsArray.forEach((blogs) => {
-        domString += `<div id="blog-'+ (i+1) +'">`;
-        domString +=   `<h3>${blogs.title}</h3>`;
-        domString +=   `<h4>${blogs.date}</h4>`;
-        domString +=   `<p>${blogs.post}</p>`;
-        domString += `</div>`
-    });           
-    writeToDom(domString, "blogs");
-};
+// function WTF(){
+//     console.log('Oops! Something went wrong.');
+// }
 
-function executeThisFunctionAfterFileLoads (){
-    const data = JSON.parse(this.responseText);
-    createBlogPosts(data.blogs);
-}
-
-function WTF(){
-    console.log('Oops! Something went wrong.');
-}
-
-const startApplication = () => {
-    let myRequest = new XMLHttpRequest();
-    myRequest.addEventListener("load", executeThisFunctionAfterFileLoads);
-    myRequest.addEventListener("error", WTF);
-    myRequest.open("GET", "javascripts/db/blogs.json");
-    myRequest.send();
-};
-
-startApplication();
+// const startApplication = () => {
+//     let myRequest = new XMLHttpRequest();
+//     myRequest.addEventListener("load", executeThisFunctionAfterFileLoads);
+//     myRequest.addEventListener("error", WTF);
+//     myRequest.open("GET", "javascripts/db/blogs.json");
+//     myRequest.send();
+// };
