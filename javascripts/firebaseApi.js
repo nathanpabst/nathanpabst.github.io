68946f0f9513877320fe837ab7b86b86129e03d6
Nathan Pabst <nathan.pabst@gmail.com>
2018-06-04 18:06:39 -0500

    wip

--- javascripts/firebaseApi.js
+++ javascripts/firebaseApi.js
@@ -4,6 +4,22 @@ const setConfig = (fbConfig) => {
   firebaseConfig = fbConfig;
 };
 
+const saveBlogPosts = (newPost) => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      method: 'POST',
+      url: `${firebaseConfig.databaseURL}/blogs.json`,
+      data: JSON.stringify(newPost),
+    })
+      .done((uniqueKey) => {
+        resolve(uniqueKey);
+      })
+      .fail((error) => {
+        reject(error);
+      });
+  });
+};
+
 module.exports = {
   setConfig,
 };
